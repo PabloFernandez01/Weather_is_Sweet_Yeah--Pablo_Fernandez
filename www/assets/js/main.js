1e5407c5eca188bd5cfd05431e6ab68d84413a49
PabloFernandez01 <pablofernandezmoriones@gmail.com>
2021-10-21 18:18:09 +0200

    Entrega final del proyecto de desarrollo V.2

--- www/assets/js/main.js
+++ www/assets/js/main.js
@@ -13,16 +13,25 @@ $("#btnMostrarMenu").on("click", function (event) {
 $("#btnHome").on("click", function (event) {
   //Evita realizar tu tarea por defcto
   event.preventDefault();
+  $("#resultado").empty();
   $("#sectionSearch").hide();
-  $("#resultado").hide();
   $("#bodyCard").show();
 });
 
 $("#btnSearch").on("click", function (event) {
   //Evita realizar tu tarea por defcto
   event.preventDefault();
+  $("#resultado").empty();
   $("#bodyCard").hide();
   $("#sectionSearch").show();
+});
+
+$("#btnLocation").on("click", function (event) {
+  $("#resultado").empty();
+  $("#sectionSearch").hide();
+  $("#bodyCard").hide();
+  navigator.geolocation.getCurrentPosition(onSuccess, onError);
+  $("#resultado").show();
 });
 
 $("#btnCity").on("click", function (event) {
@@ -44,7 +53,7 @@ function today(cityInput) {
     var icon = "";
     description = weather[0].description;
     icon = weather[0].icon;
-    icon = "img/" + icon + ".png";
+    icon = "assets/img/" + icon + ".png";
     $("#resultado").append(
       "<div class='container'>" +
         "<div class='card'>" +
@@ -69,8 +78,6 @@ function today(cityInput) {
         "<br></br>" +
         "</div>"
     );
-    /*$("#status").html(result);
-    $("#img").attr("src", icon);*/
   });
 }
 
@@ -98,7 +105,7 @@ function nextDays(cityInput) {
       for (var i = 4; i < 40; i += 8) {
         description = list[i].weather[0].description;
         icon = list[i].weather[0].icon;
-        icon = "img/" + icon + ".png";
+        icon = "assets/img/" + icon + ".png";
         var day = new Date(list[i].dt * 1000);
         var dayName = days[day.getDay()];
         $("#resultado").append(
@@ -125,9 +132,56 @@ function nextDays(cityInput) {
             "<br></br>" +
             "</div>"
         );
-        /*$("#status").html(result);
-        $("#img").attr("src", icon);*/
       }
     }
   });
+}
+
+var onSuccess = function (position) {
+  $.ajax({
+    url:
+      "https://api.openweathermap.org/data/2.5/weather?lat=" +
+      position.coords.latitude +
+      "&lon=" +
+      position.coords.longitude +
+      "&appid=21fed6f7366cfe19d24d9e23f339b81d",
+    type: "GET",
+  }).done(function (response) {
+    var weather = response.weather;
+    var description = "";
+    var icon = "";
+    var name = response.name;
+    description = weather[0].description;
+    icon = weather[0].icon;
+    icon = "assets/img/" + icon + ".png";
+    $("#resultado").append(
+      "<div class='container'>" +
+        "<div class='card'>" +
+        "<div class='row g-0'>" +
+        "<div class='col-5 col-sm-4'>" +
+        "<img src='" +
+        icon +
+        "'/>" +
+        "</div>" +
+        "<div class='col-7 col-sm-8'>" +
+        "<div class='card-body'>" +
+        "<h5 class='card-title'>" +
+        "Today in " +
+        name +
+        "</h5>" +
+        "<p class='card-text'>" +
+        description +
+        "</p>" +
+        "</div>" +
+        "</div>" +
+        "</div>" +
+        "</div>" +
+        "<br></br>" +
+        "</div>"
+    );
+  });
+};
+
+function onError(error) {
+  alert("code: " + error.code + "\n" + "message: " + error.message + "\n");
 }
